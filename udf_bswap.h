d21e718fbe17e1827be27b1351386a441db074f0
Andrew Randrianasulu <40185805+Randrianasulu@users.noreply.github.com>
2022-03-03 19:47:46 +0300

    bswap not defined on termux if using libbsd
    .. copied from qemu

--- udf_bswap.h
+++ udf_bswap.h
@@ -33,6 +33,37 @@
 #include <sys/endian.h>
 // #include <machine/bswap.h>
 // #include <sys/bswap.h>
+#include <byteswap.h>
+
+if defined(__TERMUX__) 
+
+static inline uint16_t bswap16(uint16_t x)
+{
+    return (((x & 0x00ff) << 8) |
+            ((x & 0xff00) >> 8));
+}
+
+static inline uint32_t bswap32(uint32_t x)
+{
+    return (((x & 0x000000ffU) << 24) |
+            ((x & 0x0000ff00U) <<  8) |
+            ((x & 0x00ff0000U) >>  8) |
+            ((x & 0xff000000U) >> 24));
+}
+
+static inline uint64_t bswap64(uint64_t x)
+{
+    return (((x & 0x00000000000000ffULL) << 56) |
+            ((x & 0x000000000000ff00ULL) << 40) |
+            ((x & 0x0000000000ff0000ULL) << 24) |
+            ((x & 0x00000000ff000000ULL) <<  8) |
+            ((x & 0x000000ff00000000ULL) >>  8) |
+            ((x & 0x0000ff0000000000ULL) >> 24) |
+            ((x & 0x00ff000000000000ULL) >> 40) |
+            ((x & 0xff00000000000000ULL) >> 56));
+} 
+
+#endif
 
 /* rest only relevant for big endian machines */
 #if (BYTE_ORDER == BIG_ENDIAN)
